87eba3b59bf4c2428b8e3c6ed0b56729e9f990c6
nmcgill <nmcgill@users.noreply.github.com>
2014-02-28 13:57:46 -0500

    Initial photo class

---
+++ Kluster/src/com/cs446/kluster/Photo.java
@@ -0,0 +1,15 @@
+package com.cs446.kluster;
+
+
+public class Photo {
+	private int mPhotoID;
+	/**TODO: Add as needed */
+	
+	public int getID() {
+		return mPhotoID;
+	}
+	
+	public Photo(int id) {
+		mPhotoID = id;
+	}
+}
