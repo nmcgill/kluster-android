6864e1200282fcf96dec2e01e9026c26d685bf3b
nmcgill <nmcgill@users.noreply.github.com>
2014-02-28 13:58:01 -0500

    Initial user class

---
+++ Kluster/src/com/cs446/kluster/User.java
@@ -0,0 +1,15 @@
+package com.cs446.kluster;
+
+
+public class User {
+	private int mUserID;
+	/**TODO: Add as needed */
+	
+	public int getID() {
+		return mUserID;
+	}
+	
+	public User(int id) {
+		mUserID = id;
+	}
+}
