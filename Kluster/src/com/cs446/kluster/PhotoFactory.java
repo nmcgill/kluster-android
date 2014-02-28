d30ad98d9a29206d3b85894188ba3993980ffc6f
nmcgill <nmcgill@users.noreply.github.com>
2014-02-28 17:10:50 -0500

    Added GPS data when taking a picture

--- Kluster/src/com/cs446/kluster/PhotoFactory.java
+++ Kluster/src/com/cs446/kluster/PhotoFactory.java
@@ -6,7 +6,10 @@ import java.util.Date;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,15 +17,49 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
 
-public class PhotoFactory extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+/** Handles switching to Camera, saving data, and recording GPS information */
+public class PhotoFactory extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, 
+													  GooglePlayServicesClient.OnConnectionFailedListener {
 	public static final int MEDIA_TYPE_IMAGE = 1;
 	public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	private static Uri fileUri;
-	
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private static Uri fileUri;
+    private static LocationClient mLocationClient = null;
+    
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+		// Default constructor. Sets the dialog field to null
+		public ErrorDialogFragment() {
+	    	super();
+	    	mDialog = null;
+		}
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+	    	mDialog = dialog;
+		}
+		
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+    }
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+       /* Create a new location client, using the enclosing class to
+        * handle callbacks. */
+        mLocationClient = new LocationClient(this, this, this);
+        
 		TakePhoto();
 	}
 	
@@ -39,6 +76,10 @@ public class PhotoFactory extends Activity {
                 // Image capture failed, advise user
             }
         }
+        
+        // Disconnecting the client invalidates it.
+        Log.w("gps", mLocationClient.getLastLocation().toString());
+        mLocationClient.disconnect();
     }
 	
 	public void TakePhoto() {
@@ -48,6 +89,9 @@ public class PhotoFactory extends Activity {
 	    fileUri = PhotoFactory.getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
 	    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
 
+        // Connect the location client.
+        mLocationClient.connect();
+        
 	    // start the image capture Intent
 	    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 	}
@@ -101,4 +145,66 @@ public class PhotoFactory extends Activity {
 
         return mediaFile;
     }
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            // Get the error code
+            int errorCode = connectionResult.getErrorCode();
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    errorCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getFragmentManager(),
+                        "Location Updates");
+            }
+        }
+	}
+
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();		
+	}
 }
