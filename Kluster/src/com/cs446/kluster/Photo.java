312802a6861ad4fcfa6df86a7110558cda904740
Rohaan <m.rohaan.hamid@gmail.com>
2014-02-28 17:40:20 -0500

    Added JSON reader

--- Kluster/src/com/cs446/kluster/Photo.java
+++ Kluster/src/com/cs446/kluster/Photo.java
@@ -1,24 +1,77 @@
 package com.cs446.kluster;
 
+import java.util.Date;
+
+import android.graphics.Paint.Join;
 import android.location.Location;
+import android.text.format.DateFormat;
 
 
 public class Photo {
-	private int mPhotoID;
+	private long mPhotoId;
 	private Location mLocation;
-	/**TODO: Add as needed */
+	private java.text.DateFormat mDate;
+	private long mUserId;
+	private String mUrl;
+	private String mTags;
 	
-	public int getID() {
-		return mPhotoID;
+	
+	
+	public Photo() {
+		super();
+		this.mPhotoId = -1;
+		this.mLocation = new Location("NULL");
+		this.mDate = null;
+		this.mUserId = -1;
+		this.mUrl = "";
+		this.mTags = "";
 	}
-	
-	public Location getLocation() {
+
+	public long getmPhotoId() {
+		return mPhotoId;
+	}
+
+	public void setmPhotoId(long mPhotoId) {
+		this.mPhotoId = mPhotoId;
+	}
+
+	public Location getmLocation() {
 		return mLocation;
 	}
-	
-	
-	public Photo(int id, Location loc) {
-		mPhotoID = id;
-		mLocation = loc;
+
+	public void setmLocation(Location mLocation) {
+		this.mLocation = mLocation;
+	}
+
+	public java.text.DateFormat getmDate() {
+		return mDate;
+	}
+
+	public void setmDate(java.text.DateFormat dateFormat) {
+		this.mDate = dateFormat;
+	}
+
+	public long getmUserId() {
+		return mUserId;
+	}
+
+	public void setmUserId(long mUserId) {
+		this.mUserId = mUserId;
+	}
+
+	public String getmUrl() {
+		return mUrl;
+	}
+
+	public void setmUrl(String mUrl) {
+		this.mUrl = mUrl;
+	}
+
+	public String getmTags() {
+		return mTags;
+	}
+
+	public void setmTags(String mTags) {
+		this.mTags = mTags;
 	}
 }
