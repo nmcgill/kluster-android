6e6744ce07f90c0f59d27c0056fb4848feea6799
nmcgill <nmcgill@users.noreply.github.com>
2014-03-01 14:49:34 -0500

    Initial map adapter

---
+++ Kluster/src/com/cs446/kluster/mapadapter/PhotoInfoWindowAdapter.java
@@ -0,0 +1,25 @@
+package com.cs446.kluster.mapadapter;
+
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.model.Marker;
+
+public class PhotoInfoWindowAdapter implements InfoWindowAdapter {
+
+	public PhotoInfoWindowAdapter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View getInfoContents(Marker marker) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public View getInfoWindow(Marker marker) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
