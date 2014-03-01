c27cd855e18afa3cfdb976a17fea2e97dcf2e4db
nmcgill <nmcgill@users.noreply.github.com>
2014-03-01 12:59:39 -0500

    Initial Kluster account class

--- Kluster/src/com/cs446/kluster/MainActivity.java
+++ Kluster/src/com/cs446/kluster/MainActivity.java
@@ -1,14 +1,8 @@
 package com.cs446.kluster;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.FragmentManager;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,12 +11,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
-    // An account type, in the form of a domain name
-    public static final String ACCOUNT_TYPE = "kluster.com";
-    // The account name
-    public static final String ACCOUNT = "dummyaccount";
-    
+public class MainActivity extends Activity {    
     // Instance fields
     Account mAccount;
 	PhotoFactory mFactory;
@@ -32,6 +21,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); 
         
+        
+        //** TODO: Move account creation to main? */
         // Create account for this device
         // /**TODO*/
         //mAccount = CreateSyncAccount(this);
@@ -69,43 +60,7 @@ public class MainActivity extends Activity {
 		});
     }
     
-    /**
-     * Create a new dummy account for the sync adapter
-     *
-     * @param context The application context
-     */
-    public static Account CreateSyncAccount(Context context) {
-        // Create the account type and default account
-        Account newAccount = new Account(
-                ACCOUNT, ACCOUNT_TYPE);
-        // Get an instance of the Android account manager
-        AccountManager accountManager =
-                (AccountManager) context.getSystemService(
-                        ACCOUNT_SERVICE);
-        /*
-         * Add the account and account type, no password or user data
-         * If successful, return the Account object, otherwise report an error.
-         */
-        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
-            /*
-             * If you don't set android:syncable="true" in
-             * in your <provider> element in the manifest,
-             * then call context.setIsSyncable(account, AUTHORITY, 1)
-             * here.
-             */
-        	ContentResolver.setIsSyncable(newAccount, PhotoProvider.PROVIDER_NAME, 1);
-        	return newAccount;
-        } else {
-            /*
-             * The account exists or some other error occurred. Log this, report it,
-             * or handle it internally.
-             */
-        	ArrayList<Account> accounts = new ArrayList<Account>(Arrays.asList(accountManager.getAccounts()));
-        	
-        	return accounts.get(accounts.indexOf(newAccount));
-        }
-    }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
