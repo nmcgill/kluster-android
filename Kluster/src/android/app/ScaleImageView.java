e496229dd02803aec8a5c5b38c7342c7252b2fbb
nmcgill <nmcgill@users.noreply.github.com>
2014-03-31 13:04:59 -0400

    Imageview that fills the screen with aspect ratio maintained

--- Kluster/src/android/app/ScaleImageView.java
+++ Kluster/src/android/app/ScaleImageView.java
@@ -23,31 +23,23 @@ public class ScaleImageView extends ImageView {
   }
 
   @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    try {
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+  {
       Drawable drawable = getDrawable();
-      if (drawable == null) {
-        setMeasuredDimension(0, 0);
-      } else {
-        int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int measuredHeight = MeasureSpec.getSize(heightMeasureSpec);
-        if (measuredHeight == 0 && measuredWidth == 0) { //Height and width set to wrap_content
-          setMeasuredDimension(measuredWidth, measuredHeight);
-        } else if (measuredHeight == 0) { //Height set to wrap_content
-          int width = measuredWidth;
-          int height = width *  drawable.getIntrinsicHeight() / drawable.getIntrinsicWidth();
-          setMeasuredDimension(width, height);
-        } else if (measuredWidth == 0){ //Width set to wrap_content
-          int height = measuredHeight;
-          int width = height * drawable.getIntrinsicWidth() / drawable.getIntrinsicHeight();
-          setMeasuredDimension(width, height);
-        } else { //Width and height are explicitly set (either to match_parent or to exact value)
-          setMeasuredDimension(measuredWidth, measuredHeight);
-        }
+      if (drawable != null)
+      {
+          int width =  MeasureSpec.getSize(widthMeasureSpec);
+          int diw = drawable.getIntrinsicWidth();
+          if (diw > 0)
+          {
+              int height = width * drawable.getIntrinsicHeight() / diw;
+              setMeasuredDimension(width, height);
+          }
+          else
+              super.onMeasure(widthMeasureSpec, heightMeasureSpec);
       }
-    } catch (Exception e) {
-      super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
+      else
+          super.onMeasure(widthMeasureSpec, heightMeasureSpec);
   }
 
 }
